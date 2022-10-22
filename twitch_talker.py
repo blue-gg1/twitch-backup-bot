095174c63114ee6da3b6c46cee5e18700f7bb8ea
root <root@mainframe.local>
2022-10-22 22:03:41 +0300

    messing with the ablity to parse text

--- twitch_talker.py
+++ twitch_talker.py
@@ -1,6 +1,7 @@
 import os
 import requests
 import regex
+import subprocess
 from settings import twitch_channel, rss_feed_gen
 
 print(twitch_channel)
@@ -20,3 +21,14 @@ rss_data_string = str(rss_data.content)
 write_rss_disk = open("feed.rss", "w")
 write_rss_disk.write(rss_data_string)
 write_rss_disk.close()
+
+video_id_list = subprocess.run(["bash", "/home/rod/Documents/code/twitch-backup-bot/rss_reader.sh"])
+
+print(type(video_id_list))
+str(video_id_list)
+print(type(video_id_list))
+
+video_id_list.split(sep=None, maxsplit=-1)
+
+for i in video_id_list:
+    print(i)
