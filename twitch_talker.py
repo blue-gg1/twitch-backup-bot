9cfabbf65ff435485acf56d5262f713485412dfa
root <root@mainframe.local>
2022-10-22 17:20:21 +0300

    added the basics

--- twitch_talker.py
+++ twitch_talker.py
@@ -1,14 +1,25 @@
-class TwitchRecorder:  
-  
-    def __init__(self):  
-        # global configuration  
-        self.client_id = "jzkbprff40iqj646a697cyrvl0zt2m6" # don't change this  
-        # get oauth token value by typing `streamlink --twitch-oauth-authenticate` in terminal  
-        self.oauth_token = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"  
-        self.ffmpeg_path = 'ffmpeg'  
-        self.refresh = 30.0  
-        self.root_path = "/Users/junian/Documents/twitch"  
+import os
+import requests
+# import youtube-dl
 
-        # user configuration  
-        self.username = "juniantr"  
-        self.quality = "best"
+twitch_channel = 'trashfuturepodcast'
+twitch_url = 'https://www.twitch.tv/'+twitch_channel
+
+print(twitch_channel)
+print(twitch_url)
+
+
+# class TwitchRecorder:  
+  
+#     def __init__(self):  
+#         # global configuration  
+#         self.client_id = "jzkbprff40iqj646a697cyrvl0zt2m6" # don't change this  
+#         # get oauth token value by typing `streamlink --twitch-oauth-authenticate` in terminal  
+#         self.oauth_token = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"  
+#         self.ffmpeg_path = 'ffmpeg'  
+#         self.refresh = 30.0  
+#         self.root_path = "/Users/junian/Documents/twitch"  
+
+#         # user configuration  
+#         self.username = "juniantr"  
+#         self.quality = "best"
