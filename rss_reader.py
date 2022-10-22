4605da8a3d0786e5aa24d39d93e5d3d67e2b53e9
root <root@mainframe.local>
2022-10-22 20:53:16 +0300

    made it work in bash

--- rss_reader.py
+++ rss_reader.py
@@ -7,6 +7,8 @@ from settings import twitch_video_regex
 load_rss = open("feed.rss" , "r")
 # print(load_rss.read())
 
+print(twitch_video_regex)
+
 urls_only = regex.match(twitch_video_regex, load_rss.read())
 print(urls_only)
 
---
+++ rss_reader.sh
@@ -0,0 +1,1 @@
+cat feed.rss | egrep -o "https://www.twitch.tv/videos/([0-9]{10})" | sort | uniq
