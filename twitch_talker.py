8548dd3479e1486cf604124cd8c1a61d0a2de3b5
root <root@mainframe.local>
2022-10-22 20:27:10 +0300

    removed the old code

--- twitch_talker.py
+++ twitch_talker.py
@@ -14,28 +14,3 @@ rss_data = requests.get(rss_url)
 
 print(rss_data.status_code)
 print(rss_data.content)
-
-# main_page = requests.get(twitch_url)
-# print(main_page.status_code)
-# print(main_page.content)
-
-# write_html = open("page.html", "w")
-
-# make_html_str = str(main_page.content)
-# write_html.write(make_html_str)
-# write_html.close()
-
-# class TwitchRecorder:  
-  
-#     def __init__(self):  
-#         # global configuration  
-#         self.client_id = "jzkbprff40iqj646a697cyrvl0zt2m6" # don't change this  
-#         # get oauth token value by typing `streamlink --twitch-oauth-authenticate` in terminal  
-#         self.oauth_token = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"  
-#         self.ffmpeg_path = 'ffmpeg'  
-#         self.refresh = 30.0  
-#         self.root_path = "/Users/junian/Documents/twitch"  
-
-#         # user configuration  
-#         self.username = "juniantr"  
-#         self.quality = "best"
