4cef65bc4ce5afc2fd3ef9a1407d3a83e7ec4429
root <root@mainframe.local>
2022-10-22 19:42:22 +0300

    added more settings and so on

--- settings.py
+++ settings.py
@@ -1,10 +1,8 @@
 # one place for strings, functions, and dics
-import requets
+import requests
 
 auth_header = 'Bearer 2gbdx6oar67tqtcmt49t3wpcgycthx'
 client_header = 'Client-Id: uo6dggojyb8d6soh92zknwmi5ej1q2'
 api_url = 'https://api.twitch.tv/helix/videos'
 twitch_channel = 'trashfuturepodcast'
-
-GET https://api.twitch.tv/helix/users?login=<username>
-
+graph_ql= 'https://gql.twitch.tv/gql'
