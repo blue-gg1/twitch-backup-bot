9270a6b428014da48a8557a111ebc3be040c1e2f
root <root@mainframe.local>
2022-10-22 20:36:09 +0300

    made the RSS work

--- twitch_talker.py
+++ twitch_talker.py
@@ -14,3 +14,9 @@ rss_data = requests.get(rss_url)
 
 print(rss_data.status_code)
 print(rss_data.content)
+
+rss_data_string = str(rss_data.content)
+
+write_rss_disk = open("feed.rss", "w")
+write_rss_disk.write(rss_data_string)
+write_rss_disk.close()
