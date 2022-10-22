997ea599d5206106ff7a7d0b8d0a2a4a78282f3f
root <root@mainframe.local>
2022-10-22 21:52:09 +0300

    updated the shelll file

--- rss_reader.sh
+++ rss_reader.sh
@@ -1,1 +1,3 @@
-cat feed.rss | egrep -o "https://www.twitch.tv/videos/([0-9]{10})" | sort | uniq
+# cat feed.rss | egrep -o "https://www.twitch.tv/videos/([0-9]{10})" | sort | uniq
+
+grep -o -E 'https?:\/\/(www\.twitch.tv)\/videos\/([0-9]{10})' feed.rss 2> /dev/null | uniq
