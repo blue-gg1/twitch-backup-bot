2bc600c821f51d2846729def9ed490a81c473cfc
root <root@mainframe.local>
2022-10-22 21:49:10 +0300

    the list is back, still does not work

--- rss_reader.py
+++ rss_reader.py
@@ -6,25 +6,21 @@ from settings import twitch_video_regex, twitch_video_id_regex
 
 load_rss = open("feed.rss" , "r")
 rss_content = load_rss.read()
-print(twitch_video_id_regex)
+print(twitch_video_regex)
 
 
-print(regex.search(twitch_video_id_regex, rss_content))
+print(regex.search(twitch_video_regex, rss_content))
+print(regex.findall(twitch_video_regex, rss_content))
 
-print(regex.findall(twitch_video_id_regex, rss_content))
 
-rss_findings = regex.findall(twitch_video_id_regex, rss_content)
-rss_findings = list(dict.fromkeys(rss_findings))
+rss_findings = regex.findall(twitch_video_regex, rss_content)
+uniq = list(dict.fromkeys(rss_findings))
 
-for i in rss_findings:
+for i in uniq:
     print(i)
 
+
 # print(regex.findall(pattern, string)(twitch_video_regex, rss_content))
-
-
-
 # urls_only = regex.match(twitch_video_regex, load_rss.read())
 # print(urls_only)
-
-
 # print(load_rss.read().find(twitch_video_regex))
