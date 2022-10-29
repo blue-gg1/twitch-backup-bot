490111ecd7ff7270de69c13f553de51244a01d52
root <root@mainframe.local>
2022-10-29 20:26:17 +0300

    putting the code in a function

--- rss_fetch.py
+++ rss_fetch.py
@@ -7,21 +7,28 @@ from settings import twitch_channel, rss_feed_gen, date_now_string, rss_file_nam
 print(twitch_channel)
 print(rss_feed_gen)
 
-rss_url = rss_feed_gen + twitch_channel
 
 print(rss_url)
 
 rss_data = requests.get(rss_url)
 
-rss_data_string = str(rss_data.content)
-write_rss_disk = open(rss_file_name, "w")
-write_rss_disk.write(rss_data_string)
-write_rss_disk.close()
 
-video_id_list = subprocess.call(["bash", "/home/rod/Documents/code/twitch-backup-bot/rss_reader.sh"])
 print('\r\n')
 print(type(video_id_list))
 print(video_id_list)
+
+def get_rss_feed():
+    rss_url = rss_feed_gen + twitch_channel
+    rss_data = requests.get(rss_url)
+    rss_data_string = str(rss_data.content)
+    write_rss_disk = open(rss_file_name, "w")
+    write_rss_disk.write(rss_data_string)
+    write_rss_disk.close()
+    video_id_list = subprocess.call(["bash", "/home/rod/Documents/code/twitch-backup-bot/rss_reader.sh"])
+
+
+    
+
 
 # video_id_list_str = video_id_list.stdout.splitlines()
 # print(video_id_list_str)
