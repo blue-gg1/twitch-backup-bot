ac48f8f8c45d7dc0754386a0a02bc293ffa0e4b1
root <root@mainframe.local>
2022-10-29 15:05:26 +0300

    added comments

--- rss_fetch.py
+++ rss_fetch.py
@@ -13,11 +13,6 @@ print(rss_url)
 
 rss_data = requests.get(rss_url)
 
-# print(rss_data.status_code)
-# print(rss_data.content)
-
-rss_file_name
-
 rss_data_string = str(rss_data.content)
 write_rss_disk = open(rss_file_name, "w")
 write_rss_disk.write(rss_data_string)
--- settings.py
+++ settings.py
@@ -1,18 +1,21 @@
 # one place for strings, functions, and dics
+
+### deal with date and names 
 import requests
 from datetime import date
-
 date_now = date.today()
 date_now_string = str(date_now)
+## define file as the day
 rss_file_name = 'feed'+ date_now_string +'.rss'
 
+### define twitch data
 auth_header = 'Bearer 2gbdx6oar67tqtcmt49t3wpcgycthx'
 client_header = 'Client-Id: uo6dggojyb8d6soh92zknwmi5ej1q2'
 api_url = 'https://api.twitch.tv/helix/videos'
 twitch_channel = 'trashfuturepodcast'
 graph_ql= 'https://gql.twitch.tv/gql'
 
-
+### define RSS data
 rss_feed_gen = 'https://twitchrss.appspot.com/vod/'
 twitch_video_regex = 'https?:\/\/(www\.twitch.tv)\/videos\/([0-9]{10})'
 twitch_video_id_regex = '([0-9]{10})'
