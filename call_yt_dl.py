d1f09dc76ca31ce2d4f60afb201b904d13732f0d
root <root@mainframe.local>
2022-10-29 21:12:38 +0300

    failed to make work

--- call_yt_dl.py
+++ call_yt_dl.py
@@ -1,4 +1,4 @@
 import os
 import subprocess
 
-subprocess.call(["/usr/bin/youtube-dl", "-a /home/rod/Documents/code/twitch-backup-bot/urls.rss"])
+subprocess.call(["/usr/bin/youtube-dl", "-a /home/rod/Documents/code/twitch-backup-bot/urls.txt"])
