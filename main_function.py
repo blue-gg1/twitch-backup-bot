67ec53ee5b0d0cc0b7cd7a547a5a5dfbc5433e24
root <root@mainframe.local>
2022-11-02 20:41:35 +0200

    brought the bot call back

--- main_function.py
+++ main_function.py
@@ -1,4 +1,4 @@
-from libs.rss_reader import send_to_telegram
+from libs. import send_to_telegram
 from libs.rss_fetch import get_rss_feed
 import time
 
@@ -9,5 +9,5 @@ take_file = open('urls.rss' , 'r')
 # print(take_file.readlines())
 for i in take_file.readlines():
     print(i)
-    send_to_telegram(i)
+    send_to_telegram('/VideoDownloadBot '+i)
     # time.sleep(1)
