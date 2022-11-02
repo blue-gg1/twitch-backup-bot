fae82e3fa85b5b1c2b98533ab97f14b9740806cd
root <root@mainframe.local>
2022-11-02 19:24:20 +0200

    messing with the functions

--- main_function.py
+++ main_function.py
@@ -1,5 +1,8 @@
 from rss_reader import send_to_telegram
 from rss_fetch import get_rss_feed
+import time
+
+
 
 get_rss_feed()
 take_file = open('urls.rss' , 'r')
@@ -7,5 +10,3 @@ take_file = open('urls.rss' , 'r')
 for i in take_file.readlines():
     print(i)
     send_to_telegram('/VideoDownloadBot '+i)
-    time.sleep(1)
-
