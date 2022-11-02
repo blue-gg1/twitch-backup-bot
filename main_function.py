ba5f0f6484d7a4d6447b41c7fb0e8b3cdf26535c
root <root@mainframe.local>
2022-11-02 21:17:29 +0200

    does it work better? who knows?

--- main_function.py
+++ main_function.py
@@ -8,5 +8,5 @@ take_file = open('urls.rss' , 'r')
 # print(take_file.readlines())
 for i in take_file.readlines():
     print(i)
-    send_to_telegram(i)
+    send_to_telegram('/VideoDownloadBot '+i)
     # time.sleep(1)
