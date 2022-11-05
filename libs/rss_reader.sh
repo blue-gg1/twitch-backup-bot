731c513e2529ab66d9a8d843a6175575196e634a
root <root@mainframe.local>
2022-11-05 15:09:13 +0200

    fixed the regex. now will work with vid IDs that are 8+ digits.

--- libs/rss_reader.sh
+++ libs/rss_reader.sh
@@ -1,4 +1,5 @@
 # cat feed.rss | egrep -o "https://www.twitch.tv/videos/([0-9]{10})" | sort | uniq
 
 # grep -o -E 'https?:\/\/(www\.twitch.tv)\/videos\/([0-9]{10})' libs/feed.rss 2> /dev/null | uniq > libs/urls.rss
-grep -o -E 'https?:\/\/(www\.twitch.tv)\/videos\/([0-9]{9})' libs/feed.rss 2> /dev/null | uniq > libs/urls.rss
+# grep -o -E 'https?:\/\/(www\.twitch.tv)\/videos\/([0-9]{9})' libs/feed.rss 2> /dev/null | uniq > libs/urls.rss
+grep -o -E 'https?:\/\/(www\.twitch.tv)\/videos\/([0-9]{8,})' libs/feed.rss 2> /dev/null | uniq > libs/urls.rss
