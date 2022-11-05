4050d0f3e96e0a7d6ada8c62b0d62a4a2339583b
root <root@mainframe.local>
2022-11-05 14:39:24 +0200

    added some comments and made it remove the RSS files

--- main_function.py
+++ main_function.py
@@ -1,12 +1,25 @@
 from libs.rss_reader import send_to_telegram
 from libs.rss_fetch import get_rss_feed
 import time
+import os
 
+# call the functions to get twtich videos
 get_rss_feed()
 
+# send the URLs to telegram
 take_file = open('libs/urls.rss' , 'r')
 # print(take_file.readlines())
 for i in take_file.readlines():
     # print(i)
     send_to_telegram('/VideoDownloadBot '+i)
     time.sleep(5)
+
+# remove the RSS files 
+if os.path.exists("libs/feed.rss"):
+  os.remove("libs/feed.rss")
+else:
+  print("The file does not exist")
+if os.path.exists("libs/urls.rss"):
+  os.remove("libs/urls.rss")
+else:
+  print("The file does not exist")
