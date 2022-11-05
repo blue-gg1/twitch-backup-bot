13fb15315ccab9f1a7b999e8daf10132bb7e33ec
root <root@mainframe.local>
2022-11-05 15:06:12 +0200

    older twitch videos have 9 digit id and not 10. had to fix regex for that

--- libs/rss_reader.sh
+++ libs/rss_reader.sh
@@ -1,3 +1,4 @@
 # cat feed.rss | egrep -o "https://www.twitch.tv/videos/([0-9]{10})" | sort | uniq
 
-grep -o -E 'https?:\/\/(www\.twitch.tv)\/videos\/([0-9]{10})' libs/feed.rss 2> /dev/null | uniq > libs/urls.rss
+# grep -o -E 'https?:\/\/(www\.twitch.tv)\/videos\/([0-9]{10})' libs/feed.rss 2> /dev/null | uniq > libs/urls.rss
+grep -o -E 'https?:\/\/(www\.twitch.tv)\/videos\/([0-9]{9})' libs/feed.rss 2> /dev/null | uniq > libs/urls.rss
